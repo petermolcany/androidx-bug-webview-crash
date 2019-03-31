8e1a3d6e560b12afb994be69252ed19682ee0345
Peter Molcany <peter.molcany@innovatrics.com>
2019-03-31 17:20:23 +0200

    initial commit

---
+++ app/src/main/java/com/innovatrics/android/webviewbugdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.innovatrics.android.webviewbugdemo
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
